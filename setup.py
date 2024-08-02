5b0126c3af4a7f3d92e484e9e24cda47d3d80e81
ituki0426 <yuzurihainori315@gmail.com>
2024-08-02 17:41:33 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lstm',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo for the project LSTM.',
+    author='ituki shibanuma',
+    license='MIT',
+)
---
+++ src/core.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+core.py
+
+Created on `2024-08-02
+by ituki shibanuma
+yuzurihainori315@gmail.com
+"""
+import logging as log
+
+def main():
+    # TODO:
+    # run its processes
+    pass
+
+if __name__ == "__main__":
+    log.basicConfig(level=log.DEBUG,
+                    format='%(asctime)s %(message)s',
+                    datefmt="%b %d %H:%M:%S %Z")
+
+    main()
